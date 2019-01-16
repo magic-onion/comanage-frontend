65266d52fbf239a749f48411765a09f79fb4a40a
magic-onion <alexander.m.truong@gmail.com>
2019-01-16 10:53:44 -0500

    added create methods for room and member

---
+++ src/actions/room.js
@@ -0,0 +1,21 @@
+import { setCommunity } from './community'
+
+export const createNewroom = (roomObj) => {
+  return (dispatch => {
+    let config = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${localStorage.getItem('token')}`
+      },
+      body: JSON.stringify(roomObj)
+    }
+      console.log(config)
+    fetch(`http://localhost:3000/api/v1/rooms`, config).then(r=>r.json()).then(p=> {
+      let reducerBody = {community: p.community}
+      dispatch(setCommunity(reducerBody))
+    })
+  })
+}
+
+//The data is served.
--- src/containers/RoomContainer.js
+++ src/containers/RoomContainer.js
@@ -2,14 +2,53 @@ import React from 'react'
 import RoomCard from '../components/RoomCard'
 import MemberFeed from '../components/MemberFeed'
 import { connect } from 'react-redux'
+import { createNewroom } from '../actions/room'
 
+  const init = {
+    toggleCreateForm: false,
+    name: "name",
+    occupancy: 1
+  }
 
 class RoomContainer extends React.Component {
+
+  state = init
+
+  roomInput = event => this.setState({[event.target.name]: event.target.value})
+
+  createRoom = event => {
+    event.preventDefault()
+    let roomBody = {
+      room: {
+        name: this.state.name,
+        occupancy: this.state.occupancy,
+        communityId: this.props.community.id
+      }
+    }
+    this.props.createNewroom(roomBody)
+  }
+
+  get createNewRoomForm() {
+    return (
+      <form onSubmit={this.createRoom}>
+        <input onChange={this.roomInput} name="name" type="text" value={this.state.name}/>
+        <input onChange={this.roomInput} name="occupancy" type="number" value={this.state.occupancy}/>
+        <button type="submit">Save</button>
+      </form>
+    )
+  }
+
+  toggleCreateForm = () => {
+    let toggleCreateForm = !this.state.toggleCreateForm
+    this.setState({toggleCreateForm})
+  }
 
   render() {
     return (
       <div className="room-container">
       <h2>ROOM CONTAINER</h2>
+      <button onClick={this.toggleCreateForm}>Create a Room</button>
+      {this.state.toggleCreateForm ? this.createNewRoomForm : null}
         {this.props.community.rooms.map((room, i) => <RoomCard key={i} room={room} roomMembers={this.props.community.roomMembers} members={this.props.community.members}/> )}
       {this.props.community.rooms.length > 0 ?  <MemberFeed members={this.props.community.members} rooms={this.props.community.rooms} communityId={this.props.community.id}/> : null}
       </div>
@@ -25,5 +64,10 @@ const mapStateToProps = state => {
   }
 }
 
+const mapDispatchToProps = dispatch => {
+  return {
+    createNewroom: obj => dispatch(createNewroom(obj))
+  }
+}
 
-export default connect(mapStateToProps)(RoomContainer)
+export default connect(mapStateToProps, mapDispatchToProps)(RoomContainer)
